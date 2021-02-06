533f9cc5dfb5cd4ceb42c71edf4fb4f30085a22c
Седов Алексей <aleks.sedov@mail.ru>
2021-02-06 16:20:01 +0300

    added new config

--- webpack.config.js
+++ webpack.config.js
@@ -1,183 +1,90 @@
+const webpack = require('webpack');
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const TerserWebpackPlugin = require('terser-webpack-plugin');
-const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer');
-const ImageminPlugin = require('imagemin-webpack');
 
 const isDev = process.env.NODE_ENV === 'development';
 const isProd = !isDev;
 
-const optimization = () => {
-  const config = {
+const filename = (ext) => (isDev ? `[name].${ext}` : `[name].[hash].${ext}`);
+
+module.exports = {
+  mode: 'development',
+  context: path.resolve(__dirname, 'src'),
+  entry: './index.ts',
+  output: {
+    filename: filename('js'),
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.json'],
+  },
+  optimization: {
     splitChunks: {
-      chunks: 'all',                                                // Нужна при нескольких точках входа. Позволяет не грузить общий код в каждый бандл, а выносить его в отдельные файлы
+      chunks: 'all',
     },
-  };
-
-  if (isProd) {
-    config.minimizer = [                                            // В режиме прода минифицируем css
-      new OptimizeCssAssetsPlugin(),
-      new TerserWebpackPlugin(),
-    ];
-  }
-
-  return config;
-};
-
-const cssLoaders = (extra) => {
-  const loaders = [
-    {
-      loader: MiniCssExtractPlugin.loader,
-      options: {
-        hrm: isDev,
-        reloadAll: true,
-      },
-    },
-    'css-loader',
-  ];
-
-  extra && loaders.push(extra);
-
-  return loaders;
-};
-
-const jsLoader = () => {
-  const loader = [{
-    loader: 'babel-loader',
-  }];
-
-  isDev && loader.push('eslint-loader');                            // Для того чтоб линтер не применялся в продакшене и на файлах отличных от .js
-
-  return loader;
-};
-
-const filename = (ext) => isDev
-  ? `[name].${ext}`
-  : `[name].[hash].${ext}`;                                         // [name] - имя ключа === имя файла после сборки, [hash] - уникальный хэш для файла
-
-const plugins = () => {
-  const base = [                                                    // Плагины. Это классы расширяющие функционал вебпака. Всегда объявляются через new
-    new HTMLWebpackPlugin({                                 // Для создания index.html в папке прода с указанием путей до подключенных скриптов
-      template: './index.html',                                     // Указывает какой файл применять как шаблон
+  },
+  devServer: {
+    port: 4200,
+    compress: isProd,
+    open: true,
+    overlay: true,
+    hot: isDev,
+    contentBase: path.resolve(__dirname, 'dist'),
+    watchContentBase: true,
+  },
+  target: isDev ? 'web' : 'browserslist',
+  devtool: false,
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './index.html',
       minify: {
-        collapseWhitespace: isProd,                                 // Минификация html, только в режиме продакшена
+        collapseWhitespace: isProd,
+        removeComments: isProd,
+        removeRedundantAttributes: isProd,
+        useShortDoctype: isProd,
       },
     }),
-    new CleanWebpackPlugin(),                                       // Чистит папку прода при пересборке
-    new CopyWebpackPlugin({                                 // Для переноса файлов в папку прода. Для каждого файла/папки создается свой объект
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
       patterns: [
         {
-          from: path.resolve(__dirname, 'src/assets/favicon.ico'),
-          to: path.resolve(__dirname, 'public'),
+          from: path.resolve(__dirname, 'src/favicon.ico'),
+          to: path.resolve(__dirname, 'dist'),
         },
       ],
     }),
-    new MiniCssExtractPlugin({                              // Вынос стилей в отдельный файл
-      filename: filename('css'),                                // Имя этого файла
-    }),
-  ];
-
-  isProd && base.push(new BundleAnalyzerPlugin());
-
-  return base;
-};
-
-const fileLoaders = () => {
-  const loaders = ['file-loader'];
-  isProd && loaders.push({
-    loader: ImageminPlugin.loader,
-    options: {
-      bail: false,
-      cache: false,
-      imageminOptions: {
-        plugins: [
-          ['pngquant', {quality: [0.5, 0.5]}],
-          ['mozjpeg', {quality: 75, progressive: true}],
-          ['gifsicle', {interlaced: true}],
-          [
-            'svgo',
-            {
-              plugins: [{
-                removeViewBox: false,
-              }],
-            },
-          ],
-        ],
-      },
-    },
-  });
-
-  return loaders;
-};
-
-module.exports = {
-  context: path.resolve(__dirname, 'src'),                          // Контекст для всех урлов в настройках
-  mode: 'development',                                              // Режим сборки 'development' и 'production'
-  entry: {                                                          // Точки входа
-    main: ['@babel/polyfill', './index.js'],
-    analytics: './analytic.ts',
-  },
-  output: {                                                         // Выгрузка бандла
-    filename: filename('js'),                                   // Имена файлов
-    path: path.resolve(__dirname, 'public'),                        // Путь до папки выгрузки
-  },
-  resolve: {
-    extensions: ['.js', '.ts', '.json'],                            // Расширения, которые можно не указывать в импортах
-    alias: {                                                        // Алиасы, для упрощенного доступа к указанным путям. Вместо ../../../.. и т.д
-      '@': path.resolve(__dirname, 'src'),
-      '@modules': path.resolve(__dirname, 'src/modules'),
-    },
-  },
-  optimization: optimization(),
-  devServer: {                                                      // Локальный сервер. Нужен для обновления изменений в режиме реального времени
-    port: 4200,                                                     // Порт для запуска локального сервера
-    hot: isDev,
-    hotOnly: false,
-    liveReload: isDev,
-    watchContentBase: true,
-  },
-  devtool: isDev ? 'source-map' : '',                               // Создаем исходные карты, чтобы видеть код до компиляции babel'ом
-  plugins: plugins(),
-  module: {                                                         // Модули. Позволяют работать с различными типами файлов
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  module: {
     rules: [
       {
         test: /\.css$/,
-        use: cssLoaders(),
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.s[ac]ss$/,
-        use: cssLoaders('sass-loader'),
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
         test: /\.(png|jpg|svg|gif)$/,
-        use: fileLoaders(),
+        type: 'asset/resource',
       },
       {
         test: /\.(ttf|woff|woff2|eot)$/,
-        use: ['file-loader'],
-      },
-      {
-        test: /\.xml$/,
-        use: ['xml-loader'],
-      },
-      {
-        test: /\.csv$/,
-        use: ['csv-loader'],
+        type: 'asset/resource',
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: jsLoader(),
+        use: ['babel-loader'],
       },
       {
         test: /\.ts$/,
         exclude: /node_modules/,
-        loader: jsLoader(),
-      },
-    ],
+        use: ['babel-loader', 'ts-loader'],
+      }
+    ]
   },
 };
